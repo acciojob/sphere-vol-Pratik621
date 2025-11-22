53971810e653850fc7188f6d5aea8a12e731d317
Pratik Mane <manepratik3727@gmail.com>
2025-11-22 23:48:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
+
+
 function volume_sphere() {
     //Write your code here
-  
+	const radius=document.querySelector("#radius").value;
+	let r=radius*1;
+	let volume=(4/3) * Math.PI * r * r * r;
+    return volume;
 } 
 
-window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
+const calbtn=document.querySelector("#submit");
+calbtn.addEventListener("click",(e)=>{
+	e.preventDefault();
+	let ans=volume_sphere();
+	const volume=document.querySelector("#volume");
+	volume.value=ans
+	
+	
+})
+
+
+// window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
