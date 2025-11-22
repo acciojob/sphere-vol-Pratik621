e0309926d44f7a1d6e7e78e1129600a8c445ad2d
Pratik Mane <manepratik3727@gmail.com>
2025-11-22 23:52:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,10 +2,13 @@
 
 function volume_sphere() {
     //Write your code here
+	
 	const radius=document.querySelector("#radius").value;
+	// if(typeof(radius)===number)
 	let r=radius*1;
+	
 	let volume=(4/3) * Math.PI * r * r * r;
-    return volume;
+    return volume.toFixed(4) 
 } 
 
 const calbtn=document.querySelector("#submit");
